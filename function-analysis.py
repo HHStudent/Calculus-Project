31ecf2c24c7b854a3f8fa256d47eb4ad058e7267
HHStudent <dimitrisomoff@hanovernorwichschools.org>
2016-11-29 18:53:05 -0500

    Updated from Brython Server: 11/29/2016 6:53:05 PM

--- function-analysis.py
+++ function-analysis.py
@@ -20,23 +20,37 @@ def derivative(x):
     slope = rise / run
     return slope
 
-print(derivative(-3))
-print(derivative(-2))
-print(derivative(-1))
-print(derivative(0))
-print(derivative(1))
-print(derivative(2))
-print(derivative(3))
+print(round(derivative(-3),5))
+print(round(derivative(-2),5))
+print(round(derivative(-1),5))
+print(round(derivative(0),5))
+print(round(derivative(1),5))
+print(round(derivative(2),5))
+print(round(derivative(3),5))
 
 
+def second(x):
+    g = 1/1000000000
+    top = f(x + 2*g) - f(x + g) - f(x + g) + f(x)
+    bottom = g*g
+    answers = top / bottom
+    return answers
 
+print(second(-1))
+
+"""
 def secondderivative(x):
-    h = 1/100000
-    rise = f(x + h) - f(x)
-    run = h
-    slope = rise / run
+    h = 1/1000000000
+    rise = f(x + h + h) - f(x + h) - f(x + h) + f(x)
+    rise2 = (f(x + (2*h))) - (2*(f(x + h))) + f(x)
+    first = f(x + h) + f(x + h)
+    first2 = -2 * f(x + h) + f(x + 2*h) - f(x)
+    run = h*h
+    slope = first2 / run
     return slope
 
+print(secondderivative(-1))
+"""
 #for x in range [a,b]
 #    if derivative(x)>0
 #        increasing = x
